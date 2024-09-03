548f7cf2447aea48456e581df0d0405c87ab0ec9
Mikan <72257910+Mikan-DS@users.noreply.github.com>
2024-09-03 21:13:10 +0300

    настройки

--- frontend/models.py
+++ frontend/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Settings(models.Model):
+
+    gigachat_auth_data = models.CharField(max_length=500, blank=True, null=True, verbose_name="Базовая аутентификация")
+    gigachat_auth_url = models.URLField(
+        default='https://ngw.devices.sberbank.ru:9443/api/v2/oauth',
+        verbose_name="Ссыллка для авторизации")
+
+    SCOPES = [
+        (0, "GIGACHAT_API_PERS")
+    ]
+
+    gigachat_scope = models.CharField(default=0, choices=SCOPES, verbose_name="Версия API")
+
+    gigachat_access_token = models.CharField(max_length=500, blank=True, null=True, verbose_name="НЕ МЕНЯТЬ")
+    gigachat_expired_at = models.IntegerField(default=0, verbose_name="НЕ МЕНЯТЬ")
+
+    class Meta:
+        verbose_name = "Настройка"
+        verbose_name_plural = "Настройки"
+
+    def __str__(self):
+        return "Настройка"
+
+    def save(self, *args, **kwargs):
+        if not self.pk and Settings.objects.exists():
+            raise ValueError('Настройка уникальна, больше создавать нельзя!')
+
+        return super(Settings, self).save(*args, **kwargs)
---
+++ frontend/update-frontend-part.py
@@ -0,0 +1,38 @@
+import glob
+import os
+import shutil
+
+if os.path.basename(os.getcwd()) != 'i-know-the-password-frontend':
+    os.chdir('i-know-the-password-frontend')
+os.system('npm run build')
+
+with open("../template.html", "r", encoding="UTF-8") as file:
+    template = file.read()
+
+# Получаем список файлов в директории
+js_files = glob.glob('build/static/js/*.js')
+
+shutil.rmtree("../static/frontend/js/", ignore_errors=True)
+os.makedirs("../static/frontend/js/", exist_ok=True)
+# Копируем файлы в нужную директорию
+js_file = [i for i in js_files if "main" in os.path.basename(i)][0]
+shutil.copy(js_file, f'../static/frontend/js/{os.path.basename(js_file)}')
+
+# Получаем список файлов в директории
+css_files = glob.glob('build/static/css/*.css')
+
+shutil.rmtree("../static/frontend/css/", ignore_errors=True)
+os.makedirs("../static/frontend/css/", exist_ok=True)
+# Копируем файлы в нужную директорию
+css_file = [i for i in css_files if "main" in os.path.basename(i)][0]
+shutil.copy(css_file, f'../static/frontend/css/{os.path.basename(css_file)}')
+
+shutil.rmtree("../static/media", ignore_errors=True)
+shutil.copytree("build/static/media", "../static/media", dirs_exist_ok=True)
+
+with open("../templates/frontend/app.html", "w", encoding="UTF-8") as file:
+    file.write(template % (os.path.basename(js_file), os.path.basename(css_file)))
+
+os.chdir('../static')
+os.system('git add ./frontend/')
+os.system('git add ./media/')
